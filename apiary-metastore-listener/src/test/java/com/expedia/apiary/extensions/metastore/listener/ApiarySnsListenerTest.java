db96c9333a8676cd241b85439632884b5cedbac3
awoodhead <awoodhead@expedia.com>
2018-08-20 15:21:15 +0100

    first cut at a test for the listener

--- apiary-metastore-listener/src/test/java/com/expedia/apiary/extensions/metastore/listener/ApiarySnsListenerTest.java
+++ apiary-metastore-listener/src/test/java/com/expedia/apiary/extensions/metastore/listener/ApiarySnsListenerTest.java
@@ -15,12 +15,68 @@
  */
 package com.expedia.apiary.extensions.metastore.listener;
 
-import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.metastore.api.MetaException;
+import org.apache.hadoop.hive.metastore.api.Table;
+import org.apache.hadoop.hive.metastore.events.CreateTableEvent;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ApiarySnsListenerTest {
 
-    @Test
-    public void placeholder() {
-        //TODO: implement some tests
-    }
+  @Mock
+  private AmazonSNSClient snsClient;
+  @Mock
+  private Configuration configuration;
+  @Mock
+  private PublishResult publishResult;
+
+  @Captor
+  private ArgumentCaptor<PublishRequest> requestCaptor;
+
+  private String tableName = "some_table";
+  private String dbName = "some_db";
+  private ApiarySnsListener snsListener;
+
+  @Before
+  public void setup() {
+    snsListener = new ApiarySnsListener(configuration, snsClient);
+    when(snsClient.publish(any(PublishRequest.class))).thenReturn(publishResult);
+  }
+
+  @Test
+  public void onCreateTable() throws MetaException {
+    CreateTableEvent event = mock(CreateTableEvent.class);
+    when(event.getStatus()).thenReturn(true);
+    Table table = new Table();
+    table.setTableName(tableName);
+    table.setDbName(dbName);
+    when(event.getTable()).thenReturn(table);
+
+    snsListener.onCreateTable(event);
+    verify(snsClient).publish(requestCaptor.capture());
+    PublishRequest publishRequest = requestCaptor.getValue();
+    assertThat(publishRequest.getMessage(),
+        is("{\"eventType\":\"CREATE_TABLE\",\"dbName\":\"some_db\",\"tableName\":\"some_table\"}"));
+  }
+
+  // TODO: tests for other onXxx() methods
+  // TODO: test for setting ARN via environment variable
 }
