3868b239a8f3f59e368931e7de4c15649524b70f
Max Jacobs <mjacobs@hotels.com>
2019-11-18 11:14:29 +0000

    Kafka apiary metastore events (#48)
    * Creating hive-metastore-events top level module
    
    * Renaming hive-metastore-events to hive-metastore-events-parent
    
    * Added to changelog and updated readmes
    
    * Creating sns-metastore-events package
    
    * Updated changelog
    
    * Adding kafka modules
    
    * initally copying everything over
    
    * Updated references
    
    * Adding tests
    
    * Moving to common package
    
    * Updating copyright and poms
    
    * Removing unused dependency and a todo
    
    * Removing commented import
    
    * Fixed namings, exceptions, logging, poms
    
    * Final updates

---
+++ apiary-metastore-events/kafka-metastore-events/kafka-metastore-listener/src/main/java/com/expediagroup/apiary/extensions/events/metastore/KafkaProducerProperty.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2018-2019 Expedia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.expediagroup.apiary.extensions.events.metastore;
+
+import com.expediagroup.apiary.extensions.events.metastore.common.Property;
+import com.expediagroup.apiary.extensions.events.metastore.common.io.jackson.JsonMetaStoreEventSerDe;
+
+public enum KafkaProducerProperty implements Property {
+  TOPIC("topic", null),
+  BOOTSTRAP_SERVERS("bootstrap.servers", null),
+  CLIENT_ID("client.id", "ApiaryKafkaMetaStoreListener"),
+  ACKS("acks", "all"),
+  RETRIES("retries", 3),
+  MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION("max.in.flight.requests.per.connection", 1),
+  BATCH_SIZE("batch.size", 16384),
+  LINGER_MS("linger.ms", 1L),
+  BUFFER_MEMORY("buffer.memory", 33554432L),
+  SERDE_CLASS("serde.class", JsonMetaStoreEventSerDe.class.getName());
+
+  private static final String PROPERTY_PREFIX = "com.expediagroup.apiary.extensions.events.metastore.";
+
+  private final String unprefixedKey;
+  private final Object defaultValue;
+
+  private KafkaProducerProperty(String unprefixedKey, Object defaultValue) {
+    this.unprefixedKey = unprefixedKey;
+    this.defaultValue = defaultValue;
+  }
+
+  @Override
+  public String key() {
+    return new StringBuffer(PROPERTY_PREFIX).append(unprefixedKey).toString();
+  }
+
+  @Override
+  public String unPrefixedKey() {
+    return unprefixedKey;
+  }
+
+  @Override
+  public Object defaultValue() {
+    return defaultValue;
+  }
+
+  @Override
+  public String toString() {
+    return key();
+  }
+
+}
