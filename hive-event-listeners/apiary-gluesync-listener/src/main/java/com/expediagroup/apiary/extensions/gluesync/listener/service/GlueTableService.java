70f25542e77487403a7f1e81ade06bb588795c80
javsanbel2 <javiersb1997@gmail.com>
2025-05-14 15:30:57 +0200

    building

--- hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/service/GlueTableService.java
+++ hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/service/GlueTableService.java
@@ -56,6 +56,7 @@ public class GlueTableService {
         .withTableInput(transformer.transformTable(table))
         .withDatabaseName(transformer.glueDbName(table));
     try {
+      log.debug(table + " table created in glue catalog. make sure this is the version");
       glueClient.createTable(createTableRequest);
       log.debug(table + " table created in glue catalog");
     } catch (ValidationException | InvalidInputException e) {
