8c797fc3eda41099ca2245e64758d0bfe937f5ad
javsanbel2 <javiersb1997@gmail.com>
2025-05-16 14:27:20 +0200

    remove optional

--- hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
+++ hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
@@ -1,7 +1,5 @@
 package com.expediagroup.apiary.extensions.gluesync.listener.metrics;
 
-import java.util.Optional;
-
 import org.apache.hadoop.hive.common.metrics.common.Metrics;
 import org.apache.hadoop.hive.common.metrics.common.MetricsFactory;
 import org.slf4j.Logger;
@@ -11,15 +9,14 @@ public class MetricService {
 
   private static final Logger log = LoggerFactory.getLogger(MetricService.class);
 
-  public Optional<Long> incrementCounter(String name) {
+  public void incrementCounter(String name) {
     try {
       Metrics metrics = MetricsFactory.getInstance();
       if (metrics != null) {
-        return Optional.of(metrics.incrementCounter(name));
+        metrics.incrementCounter(name);
       }
     } catch (Exception e) {
       log.warn("Unable to increment counter {}", name, e);
     }
-    return Optional.empty();
   }
 }
