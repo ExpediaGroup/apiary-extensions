13580ecfb7271c58dff267ff9360c1e2cd192fbd
javsanbel2 <javiersb1997@gmail.com>
2025-05-22 16:19:45 +0200

    Add micrometer for apiary-glue-event-listener

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## 8.1.8 - 2025-05-22
+### Changed
+- Update metrics to use micrometer instead of Codahale in glue-event-listener.
+
 ## 8.1.7 - 2025-05-21
 ### Changed
 - Upgrading kafka-clients dependency to 3.9.0 which is the latest 3.x version. Also upgraded kafka-junit4 to latest version, dependency which we use for Kafka integration testing.
--- hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
+++ hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
@@ -1,19 +1,34 @@
 package com.expediagroup.apiary.extensions.gluesync.listener.metrics;
 
-import org.apache.hadoop.hive.common.metrics.common.Metrics;
-import org.apache.hadoop.hive.common.metrics.common.MetricsFactory;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 
 public class MetricService {
 
   private static final Logger log = LoggerFactory.getLogger(MetricService.class);
+  private final Map<String, Counter> metrics;
+
+  public MetricService() {
+    this.metrics = MetricConstants.LISTENER_METRICS.stream()
+        .collect(Collectors.toMap(
+            metricName -> metricName,
+            metricName -> Counter.builder(metricName).register(Metrics.globalRegistry)
+        ));
+  }
 
   public void incrementCounter(String name) {
     try {
-      Metrics metrics = MetricsFactory.getInstance();
-      if (metrics != null) {
-        metrics.incrementCounter(name);
+      Counter counter = metrics.get(name);
+      if (counter != null) {
+        counter.increment();
+      } else {
+        log.warn("Counter {} not found in Micrometer registry", name);
       }
     } catch (Exception e) {
       log.warn("Unable to increment counter {}", name, e);
