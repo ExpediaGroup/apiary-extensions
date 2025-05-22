f9941f23ab1e4b90ffeb54cd090b5addfe0d3579
javsanbel2 <javiersb1997@gmail.com>
2025-05-22 16:13:10 +0200

    add also micrometer

--- hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
+++ hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricService.java
@@ -1,17 +1,49 @@
 package com.expediagroup.apiary.extensions.gluesync.listener.metrics;
 
-import org.apache.hadoop.hive.common.metrics.common.Metrics;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.apache.hadoop.hive.common.metrics.common.MetricsFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+
 public class MetricService {
 
   private static final Logger log = LoggerFactory.getLogger(MetricService.class);
+  private final Map<String, Counter> metrics;
+
+  public MetricService() {
+    this.metrics = MetricConstants.LISTENER_METRICS.stream()
+        .collect(Collectors.toMap(
+            metricName -> metricName,
+            metricName -> Counter.builder(metricName).register(Metrics.globalRegistry)
+        ));
+  }
 
   public void incrementCounter(String name) {
+    incrementCounterInHive(name);
+    incrementCounterInMicrometer(name);
+  }
+
+  private void incrementCounterInMicrometer(String name) {
     try {
-      Metrics metrics = MetricsFactory.getInstance();
+      Counter counter = metrics.get(name);
+      if (counter != null) {
+        counter.increment();
+      } else {
+        log.warn("Counter {} not found in Micrometer registry", name);
+      }
+    } catch (Exception e) {
+      log.warn("Unable to increment counter {}", name, e);
+    }
+  }
+
+  private void incrementCounterInHive(String name) {
+    try {
+      org.apache.hadoop.hive.common.metrics.common.Metrics metrics = MetricsFactory.getInstance();
       if (metrics != null) {
         metrics.incrementCounter(name);
       }
