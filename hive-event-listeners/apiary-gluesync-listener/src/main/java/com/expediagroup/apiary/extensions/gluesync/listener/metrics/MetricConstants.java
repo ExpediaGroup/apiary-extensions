05ae3be914591365276e1eb7a9dc47bd07e615ed
Javier Sánchez Beltrán <36443689+javsanbel2@users.noreply.github.com>
2025-05-23 11:24:18 +0200

    Add micrometer for apiary-glue-event-listener (#120)

--- hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricConstants.java
+++ hive-event-listeners/apiary-gluesync-listener/src/main/java/com/expediagroup/apiary/extensions/gluesync/listener/metrics/MetricConstants.java
@@ -1,5 +1,8 @@
 package com.expediagroup.apiary.extensions.gluesync.listener.metrics;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MetricConstants {
 
   public static final String LISTENER_DATABASE_FAILURE = "glue_listener_database_failure";
@@ -8,6 +11,15 @@ public class MetricConstants {
   public static final String LISTENER_TABLE_SUCCESS = "glue_listener_table_success";
   public static final String LISTENER_PARTITION_FAILURE = "glue_listener_partition_failure";
   public static final String LISTENER_PARTITION_SUCCESS = "glue_listener_partition_success";
+
+  public static final List<String> LISTENER_METRICS = Arrays.asList(
+      LISTENER_DATABASE_FAILURE,
+      LISTENER_DATABASE_SUCCESS,
+      LISTENER_TABLE_FAILURE,
+      LISTENER_TABLE_SUCCESS,
+      LISTENER_PARTITION_FAILURE,
+      LISTENER_PARTITION_SUCCESS
+  );
 
   private MetricConstants() {}
 }
